217cc184bd9239a3f542032f7194758a56d1fe21
SEkaterina <katerina-kozyrenko@yandex.ru>
2023-09-13 09:47:19 +0300

    обновлен файл hw_4.js

--- hw/hw_4.js
+++ hw/hw_4.js
@@ -24,21 +24,36 @@
 // С помощью второго цикла перемешайте этот массив.
 // Выведите получившийся результат на экран с помощью console.log.
 
-// let array = [];
-// let count = 7;
+let array = [];
+let count = 7;
 
-// for (let i = 1; i <= count; i++) {
-//   array.push(i);
-// }
-// console.log('Исходный массив =', array);
+for (let i = 1; i <= count; i++) {
+  array.push(i);
+}
+console.log('Исходный массив =', array);
 
-// for (let i = array.length - 1; i > 0; i--) {
-//   let j = Math.floor(Math.random() * (i + 1));
-//   let temp = array[i];
-//   array[i] = array[j];
-//   array[j] = temp;
-// }
-// console.log('Перемешанный массив =', array);
+for (let i = array.length - 1; i > 0; i--) {
+  let j = Math.floor(Math.random() * (i + 1));
+  let temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+console.log('Перемешанный массив =', array);
 
 // Задача 3. С помощью цикла найдите индекс (порядковый номер) элемента массива из предыдущего задания с числом n.
 // Если такой элемент не будет найден, то выведите соответствующее сообщение на экран.
+let index = 5;
+
+array.forEach((element, index) =>
+  console.log(`Индекс: ${index}, Элемент: ${element}`)
+);
+array[index] >= 0
+  ? console.log(`Под индексом ${index} содержится элемент ${array[index]}`)
+  : console.log(`Индекс ${index} отсутствует`);
+
+// for (let i = 0; i < array.length; i++) {
+//   console.log(`Индекс: ${i}, Элемент: ${array[i]}`);
+// }
+// array[index] >= 0
+//   ? console.log(`Под индексом ${index} содержится элемент ${array[index]}`)
+//   : console.log(`Индекс ${index} отсутствует`);
