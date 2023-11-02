d32cd1cc82b6c0a9b051e2f7e307740fb4e7e170
SEkaterina <katerina-kozyrenko@yandex.ru>
2023-11-02 09:58:42 +0300

    добавлены задачиÑ с марафона htmlAcademy

--- lessons/academy.js
+++ lessons/academy.js
@@ -98,5 +98,235 @@ let getStatistics = function (players) {
 
 getStatistics(footballPlayers);
 
+// МАГАЗИН КОМПЬЮТЕРОВ
+let processorPrice = {
+    'i5': 5000,
+    'i7': 10000
+};
+
+let displayPrice = {
+    13: 5000,
+    15: 10000
+};
+
+let memoryPrice = {
+    8: 3000,
+    16: 4000
+};
+
+let buildComputer = function (memory, display, processor) {
+    let customComputer = {
+        basicPrice: 5000,
+        processor: processor,
+        display: display,
+        memory: memory,
+
+        getDescription: function () {
+            return 'компьютер с процессором ' + this.processor + ', диагональю ' + this.display + ', оперативной памятью ' + this.memory;
+        },
+
+        getPrice: function () {
+            return this.basicPrice + processorPrice[this.processor] + displayPrice[this.display] + memoryPrice[this.memory];
+        }
+    };
+
+    return customComputer;
+};
+
+let myComputer = buildComputer(8, 13, 'i7');
+console.log('В корзине ' + myComputer.getDescription() + ' стоимостью ' + myComputer.getPrice());
+
+let anotherComputer = buildComputer(16, 15, 'i5');
+console.log('В корзине ' + anotherComputer.getDescription() + ' стоимостью ' + anotherComputer.getPrice());
+
+// конфигуратор дома
+let materialPrice = {
+    'wood': 1000,
+    'stone': 1500,
+    'brick': 2000
+};
+
+let house = {
+    rooms: 10,
+    floors: 5,
+    material: 'wood',
+    coefficient: 10.5,
+
+    calculateSquare: function () {
+        return this.rooms * this.coefficient * this.floors;
+    },
+
+    calculatePrice: function () {
+        return this.calculateSquare() * materialPrice[this.material];
+    }
+};
+
+//
+var catalogData = [
+    {
+        isAvailable: true,
+        isSpecial: false
+    },
+    {
+        isAvailable: false,
+        isSpecial: false
+    },
+    {
+        isAvailable: true,
+        isSpecial: true
+    },
+    {
+        isAvailable: true,
+        isSpecial: false
+    },
+    {
+        isAvailable: false,
+        isSpecial: false
+    }
+];
+
+var updateCards = function (products) {
+    var elements = document.querySelectorAll('.product');
+
+    for (var i = 0; i < elements.length; i++) {
+
+        var element = elements[i];
+        var product = products[i];
+
+        var availabilityClass = 'product--available';
+
+        if (!product.isAvailable) {
+            availabilityClass = 'product--unavailable';
+        }
+
+        element.classList.add(availabilityClass);
+
+        if (product.isSpecial) {
+            element.classList.add('product--special');
+        }
+    }
+};
+
+updateCards(catalogData);
+
+/* Техническое задание
+
+Мяу! Помнишь магазин мороженого? Нужно создать карточки товаров, основываясь на данных, полученных с сервера.
+
+Данные — массив объектов cardsData, один элемент соответствует одному товару. У каждого объекта есть следующие свойства:
+
+- inStock — доступность товара. Если значение true — товар доступен (для такого продукта верстальщик подготовил класс good--available), если false — продукта нет в наличии (товар с классом good--unavailable).
+- imgUrl — ссылка на изображение товара.
+- text — название продукта.
+- price — цена.
+- isHit — является ли товар хитом продаж. Если значение true — продукт «хитовый». Для такого товара подготовлен класс good--hit.
+- specialOffer — специальное предложение, которое есть только у хита продаж. Должно находиться в абзаце с классом good__special-offer и быть самым последним дочерним элементом в карточке.
+
+Вот пример вёрстки одной карточки в каталоге:
+
+<ul class="goods">
+  <li class="good">
+    <h2 class="good__description">Сливочно-кофейное с кусочками шоколада</h2>
+    <img class="good__image" src="gllacy/choco.jpg" alt="Сливочно-кофейное с кусочками шоколада">
+    <p class="good__price">110₽/кг</p>
+  </li>
+  ...
+</ul>
+
+Обрати внимание, что текст в атрибуте alt у изображения должен быть таким же, как и название товара.
+
+Создай функцию renderCards, которая будет принимать на вход массив данных, вызови её, передав cardsData, и отрисуй на странице карточки мороженого.
+
+*/
+var cardsData = [
+    {
+        inStock: true,
+        imgUrl: 'gllacy/choco.jpg',
+        text: 'Сливочно-кофейное с кусочками шоколада',
+        price: 310,
+        isHit: true,
+        specialOffer: 'Двойная порция сиропа бесплатно!'
+    },
+    {
+        inStock: false,
+        imgUrl: 'gllacy/lemon.jpg',
+        text: 'Сливочно-лимонное с карамельной присыпкой',
+        price: 125,
+        isHit: false
+    },
+    {
+        inStock: true,
+        imgUrl: 'gllacy/cowberry.jpg',
+        text: 'Сливочное с брусничным джемом',
+        price: 170,
+        isHit: false
+    },
+    {
+        inStock: true,
+        imgUrl: 'gllacy/cookie.jpg',
+        text: 'Сливочное с кусочками печенья',
+        price: 250,
+        isHit: false
+    },
+    {
+        inStock: true,
+        imgUrl: 'gllacy/creme-brulee.jpg',
+        text: 'Сливочное крем-брюле',
+        price: 190,
+        isHit: false
+    }
+];
+
+var makeElement = function (tagName, className, text) {
+    var element = document.createElement(tagName);
+    element.classList.add(className);
+
+    if (text) {
+        element.textContent = text;
+    }
+    return element;
+};
+
+var createCard = function (product) {
+    var listItem = makeElement('li', 'good');
+
+    var title = makeElement('h2', 'good__description', product.text);
+    listItem.appendChild(title);
+
+    var picture = makeElement('img', 'good__image');
+    picture.src = product.imgUrl;
+    picture.alt = product.text;
+    listItem.appendChild(picture);
+
+    var price = makeElement('p', 'good__price', product.price + '₽/кг');
+    listItem.appendChild(price);
+
+    var availabilityClass = 'good--available';
+    if (!product.inStock) {
+        availabilityClass = 'good--unavailable';
+    }
+    listItem.classList.add(availabilityClass);
+
+    if (product.isHit) {
+        listItem.classList.add('good--hit');
+        var specialOffer = makeElement('p', 'good__special-offer', product.specialOffer);
+        listItem.appendChild(specialOffer);
+    }
+
+    return listItem;
+};
+
+var renderCards = function (product) {
+    var cardList = document.querySelector('.goods');
+
+    for (var i = 0; i < product.length; i++) {
+        var cardItem = createCard(product[i]);
+        cardList.appendChild(cardItem);
+    }
+};
+renderCards(cardsData);
+
+
+
 
 
